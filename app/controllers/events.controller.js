47ae46e2a824626c474fd417306c4884b7a04cea
RIZWAN\Rizwan <rizwanzaheer003@gmail.com>
2016-10-24 02:28:21 +0500

    connecting with remote database

--- app/controllers/events.controller.js
+++ app/controllers/events.controller.js
@@ -1,35 +1,71 @@
-module.exports = {
-    //show all events
-    showEvents: (req, res) => {
-        // create dummy data 
-        const events = [{
-                name: 'Basketball',
-                slug: 'basketball',
-                description: 'Throwing into a basket.'
-            },
-            {
-                name: 'Swimming',
-                slug: 'swimming',
-                description: 'Michael Phelps is the fast fish.'
-            },
-            {
-                name: 'Weightlifting',
-                slug: 'weightlifting',
-                description: 'Lifting heavy things up.'
-            }
-        ];
+const Event = require('../models/event');
 
-        // return a view with data
-        res.render('pages/events', { events: events });
-    },
-    // show a single event 
-    showSingle: (req, res) => {
-        // get a single event
-        const event = {
-            name: 'Basketball',
-            slug: 'basketball',
-            description: 'Throwing into a basket.'
-        };
-        res.render('pages/single', { event: event });
-    }
-};
+module.exports = {
+    showEvents: showEvents,
+    showSingle: showSingle,
+    seedEvents: seedEvents
+}
+
+/**
+ * show all events 
+ */
+
+function showEvents(req, res) {
+    // create dummy data 
+    // const events = [{
+    //         name: 'Basketball',
+    //         slug: 'basketball',
+    //         description: 'Throwing into a basket.'
+    //     },
+    //     {
+    //         name: 'Swimming',
+    //         slug: 'swimming',
+    //         description: 'Michael Phelps is the fast fish.'
+    //     },
+    //     {
+    //         name: 'Weightlifting',
+    //         slug: 'weightlifting',
+    //         description: 'Lifting heavy things up.'
+    //     }
+    // ];
+
+    // get all data 
+
+    // return a view with data
+    res.render('pages/events', { events: events });
+}
+// show a single event 
+function showSingle(req, res) {
+    // get a single event
+    const event = {
+        name: 'Basketball',
+        slug: 'basketball',
+        description: 'Throwing into a basket.'
+    };
+    res.render('pages/single', { event: event });
+}
+
+/**
+ * Seed the Database
+ */
+function seedEvents(req, res) {
+    // create  some event
+    const events = [
+        { name: 'Basketball', description: 'Throwing into a basket.' },
+        { name: 'Swimming', description: 'Michael Phelps is the fast fish.' },
+        { name: 'Weightlifting', description: 'Lifting heavy things up.' },
+        { name: 'ping pong', description: 'super fast paddles.' }
+    ];
+
+    // use the event model to insert/save
+    Event.remove({}, () => {
+        for (event of events) {
+            var newEvent = new Event(event);
+            newEvent.save();
+        }
+    });
+
+    // seeded!
+    res.send('Database seeded!');
+
+}
--- app/routes.js
+++ app/routes.js
@@ -14,9 +14,16 @@ router.get('/', mainController.showHome);
 
 // event router
 router.get('/events', eventsController.showEvents);
-router.get('/events/:slug', eventsController.showSingle);
+
 // router.get('/events/:slug', eventsController.showSingle);
+
+// seed event
+router.get('/events/seed', eventsController.seedEvents);
+
 
 // create events
 // edit events
 // delete events
+
+// show a single event
+router.get('/events/:slug', eventsController.showSingle);
