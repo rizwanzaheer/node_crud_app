a5a6b487667b56e1e1982680b8866e3fcf11f0e0
RIZWAN\Rizwan <rizwanzaheer003@gmail.com>
2016-10-23 18:17:47 +0500

    Routings adding ejs view engine adding main/event controllers & views

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -1,6 +1,8 @@
 module.exports = {
     showHome: (req, res) => {
         // show the home page!
+        console.log(`Hello, I'm responce: ${res}`);
         res.render('pages/home');
+
     }
 };
--- app/routes.js
+++ app/routes.js
@@ -2,10 +2,21 @@
 
 const express = require('express'),
     router = express.Router(),
-    mainController = require('./controllers/main.controller');
+    mainController = require('./controllers/main.controller'),
+    eventsController = require('./controllers/events.controller');
 
 // export Router
 module.exports = router;
 
 // define routes
+// main routes
 router.get('/', mainController.showHome);
+
+// event router
+router.get('/events', eventsController.showEvents);
+router.get('/events/:slug', eventsController.showSingle);
+// router.get('/events/:slug', eventsController.showSingle);
+
+// create events
+// edit events
+// delete events
